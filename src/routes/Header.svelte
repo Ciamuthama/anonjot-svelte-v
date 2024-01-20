fa1310ba3d1c713efa231553e7a17d5b22f7cfb3
ciamuthama <muthamapeter79@gmail.com>
2024-01-20 12:25:58 +0300

    fix: svelte-ux issue fixed plus created routes

--- src/routes/Header.svelte
+++ src/routes/Header.svelte
@@ -1,7 +1,8 @@
 <script>
 	import { page } from '$app/stores';
-	import logo from '$lib/images/svelte-logo.svg';
-	import github from '$lib/images/github.svg';
+	import logo from '$lib/images/logo.svg';
+
+	
 </script>
 
 <header>
@@ -9,6 +10,7 @@
 		<a href="https://kit.svelte.dev">
 			<img src={logo} alt="SvelteKit" />
 		</a>
+		
 	</div>
 
 	<nav>
@@ -16,14 +18,15 @@
 			<path d="M0,0 L1,2 C1.5,3 1.5,3 2,3 L2,0 Z" />
 		</svg>
 		<ul>
+			
 			<li aria-current={$page.url.pathname === '/' ? 'page' : undefined}>
 				<a href="/">Home</a>
 			</li>
+			<li aria-current={$page.url.pathname.startsWith('/create') ? 'page' : undefined}>
+				<a href="/sverdle">Create</a>
+			</li>
 			<li aria-current={$page.url.pathname === '/about' ? 'page' : undefined}>
 				<a href="/about">About</a>
-			</li>
-			<li aria-current={$page.url.pathname.startsWith('/sverdle') ? 'page' : undefined}>
-				<a href="/sverdle">Sverdle</a>
 			</li>
 		</ul>
 		<svg viewBox="0 0 2 3" aria-hidden="true">
@@ -31,12 +34,9 @@
 		</svg>
 	</nav>
 
-	<div class="corner">
-		<a href="https://github.com/sveltejs/kit">
-			<img src={github} alt="GitHub" />
-		</a>
-	</div>
 </header>
+
+
 
 <style>
 	header {
---
+++ tailwind.config.cjs
@@ -0,0 +1,18 @@
+/** @type {import('tailwindcss').Config} */
+const colors = require('tailwindcss/colors');
+
+
+module.exports = {
+  content: ['./src/**/*.{html,svelte}', './node_modules/svelte-ux/**/*.{svelte,js}'],
+  theme: {
+    extend: {
+      colors: {
+        accent: colors.blue,
+      },
+    },
+  },
+  variants: {
+    extend: {},
+  },
+  plugins: [require('svelte-ux/plugins/tailwind.cjs')],
+};
